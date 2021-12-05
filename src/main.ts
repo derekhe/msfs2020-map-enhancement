7fba3cfc44831074d6a8de6942553e5ba885d0a8
derekhe <derekhe@april1985.com>
2021-12-05 22:59:07 +0800

    Building up ui

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import naive from "naive-ui";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(naive).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <n-layout content-style="padding: 24px;">
+    <n-layout-header bordered>
+      <n-gradient-text type="info">MSFS2020 Map Enhancement</n-gradient-text>
+    </n-layout-header>
+    <n-layout-content content-style="padding: 24px;">
+      <n-space vertical justify="start" align="start">
+        <n-collapse :default-expanded-names="['1', '2', '3']">
+          <n-collapse-item title="Mod Control" name="1">
+            <n-space>
+              <n-switch>
+                <template #checked>Back to Bing Map</template>
+                <template #unchecked>Inject Google Map</template>
+              </n-switch>
+            </n-space>
+          </n-collapse-item>
+          <n-collapse-item title="Proxy Settings" name="2">
+            <n-space>
+              <n-input type="text" placeholder="IP address" />
+              <n-button>Test Access</n-button>
+            </n-space>
+          </n-collapse-item>
+          <n-collapse-item title="Google Server" name="3">
+            <n-radio-group v-model:value="selectedServer" name="radiogroup">
+              <n-space>
+                <n-radio
+                  v-for="server in googleServers"
+                  :key="server"
+                  :value="server"
+                >
+                  {{ server }}
+                </n-radio>
+              </n-space>
+            </n-radio-group>
+          </n-collapse-item>
+        </n-collapse>
+      </n-space>
+    </n-layout-content>
+    <n-layout-footer></n-layout-footer>
+  </n-layout>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
+<script>
+export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      googleServers: ["mt.google.com", "kms.google.com"],
+      selectedServer: "mt.google.com",
+    };
   },
-});
+};
 </script>
+
+<style scoped>
+.n-gradient-text {
+  font-size: 36px;
+}
+</style>
