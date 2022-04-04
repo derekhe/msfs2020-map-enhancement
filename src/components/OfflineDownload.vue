15d2f51139a7a66e62118f63ade461de6c2c7bb2
derekhe <derekhe@april1985.com>
2022-04-04 17:03:33 +0800

    Can download images.

--- src/components/OfflineDownload.vue
+++ src/components/OfflineDownload.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="offlineMap" style="height: 100%;min-height: 800px"></div>
+  <n-button @click="startDownload">Cache selected region</n-button>
 </template>
 <script>
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { defineComponent } from "vue";
-
+import got from "got";
 import "@geoman-io/leaflet-geoman-free";
 import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
 
@@ -14,17 +15,19 @@ export default defineComponent({
   components: {},
   props: {},
   data() {
-    return {};
+    return {
+      map: undefined
+    };
   },
   mounted() {
-    const map = L.map("offlineMap").setView([51.505, -0.09], 13);
+    this.map = L.map("offlineMap").setView([30.659462, 104.065735], 5);
 
     const layer = L.tileLayer("http://localhost:39871/tiles/{x}/{y}/{z}?{randint}", {
       randint: () => Math.floor(Math.random() * 200000) + 1,
       maxZoom: 20
     });
 
-    layer.addTo(map);
+    layer.addTo(this.map);
 
     const cachedLayer = L.tileLayer("http://localhost:39871/cache/{x}/{y}/{z}?{randint}", {
       randint: () => Math.floor(Math.random() * 200000) + 1,
@@ -32,14 +35,30 @@ export default defineComponent({
     });
 
     cachedLayer.setOpacity(0.3);
-    cachedLayer.addTo(map);
+    cachedLayer.addTo(this.map);
 
-    map.pm.addControls({
+    this.map.pm.addControls({
       position: "topleft",
       drawCircle: false,
       drawCircleMarker: false,
       drawMarker: false
     });
+  },
+  methods: {
+    async startDownload() {
+      const fg = L.featureGroup();
+      this.map.eachLayer((layer) => {
+        if (layer instanceof L.Path || layer instanceof L.Marker) {
+          fg.addLayer(layer);
+        }
+      });
+
+      console.log(fg.toGeoJSON());
+
+      await got.post("http://localhost:39871/offlinedownload", {
+        json: fg.toGeoJSON()
+      });
+    }
   }
 });
 
