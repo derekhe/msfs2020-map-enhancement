57cb9fb86d755d68f10ae78d4736b67d76f4f7c0
derekhe <derekhe@april1985.com>
2022-05-02 11:26:45 +0800

    Find host file

--- packages/services/hosts.ts
+++ packages/services/hosts.ts
@@ -6,10 +6,9 @@ import log from "electron-log";
 export function patchHostsFile(): void {
   log.info("Patching hosts");
 
-  let hostFilePath = "C:\\windows\\system32\\drivers\\etc\\hosts";
-  let hosts = fs.existsSync(hostFilePath)
-    ? fs.readFileSync(hostFilePath).toString()
-    : "";
+  let hostsFilePath = findHostsFile();
+  log.info(`Find host file ${hostsFilePath}`);
+  let hosts = fs.readFileSync(hostsFilePath).toString();
 
   if (hosts.indexOf("kh.ssl.ak.tiles.virtualearth.net") !== -1) {
     log.info("Already patched, skip");
@@ -18,22 +17,35 @@ export function patchHostsFile(): void {
 
   hosts +=
     "\n127.0.0.1 kh.ssl.ak.tiles.virtualearth.net\r\n127.0.0.1 khstorelive.azureedge.net\r\n";
-  fs.writeFileSync(hostFilePath, hosts, {
+  fs.writeFileSync(hostsFilePath, hosts, {
     flag: "w"
   });
   log.info("Hosts patched", hosts);
 }
 
 export function unpatchHostsFile(): void {
+  let hostsFilePath = findHostsFile();
   log.info("Unpatching hosts");
   let hosts = fs
-    .readFileSync("C:\\windows\\system32\\drivers\\etc\\hosts")
+    .readFileSync(hostsFilePath)
     .toString();
   hosts = hosts
     .replaceAll("127.0.0.1 kh.ssl.ak.tiles.virtualearth.net\r\n", "")
     .replaceAll("127.0.0.1 khstorelive.azureedge.net\r\n", "")
     .replace(/^\s*\n/gm, "");
 
-  fs.writeFileSync("C:\\windows\\system32\\drivers\\etc\\hosts", hosts);
+  fs.writeFileSync(hostsFilePath, hosts);
   log.info("Hosts unpatched", hosts);
+}
+
+function findHostsFile() {
+  for (let i = 67; i < 91; i++) {
+    const driverLetter = String.fromCharCode(i);
+    const hostPath = `${driverLetter}:\\windows\\system32\\drivers\\etc\\hosts`;
+    if (fs.existsSync(hostPath)) {
+      return hostPath;
+    }
+  }
+
+  throw new Error("Can't find host file");
 }
