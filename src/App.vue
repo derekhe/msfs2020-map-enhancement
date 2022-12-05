a025532ae938b14535eb1fd15e448ced4983c92d
derekhe <derekhe@april1985.com>
2022-12-05 22:59:07 +0800

    Building up ui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <RouterView />
 </template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
@@ -13,18 +20,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
