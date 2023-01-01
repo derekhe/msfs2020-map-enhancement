9c32a02a7b9b72780836f94359f78cfd38ade6f4
derekhe <derekhe@april1985.com>
2023-01-01 19:47:05 +0800

    start/end map server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,47 @@
 <template>
   <n-message-provider>
     <n-layout content-style="padding: 24px;">
-      <n-layout-header bordered>
-        <n-gradient-text type="info">MSFS2020 Map Enhancement</n-gradient-text>
-      </n-layout-header>
-      <n-layout-content content-style="padding: 24px;">
-        <n-space vertical justify="start" align="start">
-          <n-collapse :default-expanded-names="['1', '2', '3']">
-            <n-collapse-item title="Mod Control" name="1">
-              <n-space>
-                <n-switch @update:value="handleServerToggle" :loading="serverStarting" v-model:value="serverStarted">
+      <n-layout-content>
+        <n-card title="MSFS2020 Map Enhancement" style="margin-bottom: 16px">
+          <n-tabs type="line">
+            <n-tab-pane name="oasis" tab="Mod Control">
+              <n-space vertical size="large">
+                <n-switch
+                  @update:value="handleServerToggle"
+                  :loading="serverStarting"
+                  v-model:value="serverStarted"
+                >
                   <template #checked>Back to Bing Map</template>
                   <template #unchecked>Inject Google Map</template>
                 </n-switch>
+                <n-alert title="Health Check" type="success" v-if="healthCheckResult === 'passed'">Passed</n-alert>
+                <n-alert title="Health Check" type="info" v-if="healthCheckResult === 'checking'">Checking</n-alert>
+                <n-alert title="Health Check" type="error" v-if="healthCheckResult === 'failed'">Failed</n-alert>
               </n-space>
-            </n-collapse-item>
-            <n-collapse-item title="Proxy Settings" name="2">
+            </n-tab-pane>
+            <n-tab-pane name="the beatles" tab="Proxy Settings">
               <n-p>
-                If you need proxy to access google, please fill the proxy address.
-                Format http://ipaddress:port
+                If you need proxy to access google, please fill the proxy
+                address. Format http://ipaddress:port
               </n-p>
               <n-space>
-                <n-input v-model:value="proxyAddress" type="text" placeholder="http://ipaddress:port"
-                         @change="updateStore" />
-                <n-button @click="testProxy" v-model:loading="proxyTesting">Test Proxy</n-button>
+                <n-input
+                  v-model:value="proxyAddress"
+                  type="text"
+                  placeholder="http://ipaddress:port"
+                  @change="updateStore"
+                />
+                <n-button @click="testProxy" v-model:loading="proxyTesting">
+                  Test Proxy
+                </n-button>
               </n-space>
-            </n-collapse-item>
-            <n-collapse-item title="Google Server" name="3">
-              <n-radio-group v-model:value="selectedServer" name="radiogroup" @change="updateStore">
+            </n-tab-pane>
+            <n-tab-pane name="jay chou" tab="Map Server">
+              <n-radio-group
+                v-model:value="selectedServer"
+                name="radiogroup"
+                @change="updateStore"
+              >
                 <n-space>
                   <n-radio
                     v-for="server in googleServers"
@@ -38,9 +52,9 @@
                   </n-radio>
                 </n-space>
               </n-radio-group>
-            </n-collapse-item>
-          </n-collapse>
-        </n-space>
+            </n-tab-pane>
+          </n-tabs>
+        </n-card>
       </n-layout-content>
       <n-layout-footer></n-layout-footer>
     </n-layout>
@@ -57,7 +71,6 @@ import { HttpsProxyAgent } from "hpagent";
 const store = new Store();
 import { useMessage } from "naive-ui";
 
-
 export default defineComponent({
   name: "Home",
   data() {
@@ -67,29 +80,36 @@ export default defineComponent({
       proxyAddress: store.get("proxyAddress", null),
       proxyTesting: false,
       serverStarting: false,
-      serverStarted: false
+      serverStarted: false,
+      healthCheckResult: null
     };
   },
   setup() {
     window.$message = useMessage();
   },
   methods: {
-    handleServerToggle(value) {
+    async handleServerToggle(value) {
+      this.healthCheckResult = "checking";
       this.serverStarting = true;
 
       if (value) {
-        window.ipcRenderer.invoke(EVENT_START_SERVER, {
-          proxyAddress: this.proxyAddress,
-          selectedServer: this.selectedServer
-        }).then((result) => {
-          this.serverStarting = false;
-          if (result.success) {
-            this.serverStarted = true;
-          } else {
-            window.$message.error("Start server failed, error: " + result.error);
-            this.serverStarted = false;
-          }
-        });
+        window.ipcRenderer
+          .invoke(EVENT_START_SERVER, {
+            proxyAddress: this.proxyAddress,
+            selectedServer: this.selectedServer
+          })
+          .then(async (result) => {
+            this.serverStarting = false;
+            if (result.success) {
+              this.serverStarted = true;
+              setTimeout(await this.healthCheck, 10 * 1000);
+            } else {
+              window.$message.error(
+                "Start server failed, error: " + result.error
+              );
+              this.serverStarted = false;
+            }
+          });
       } else {
         window.ipcRenderer.invoke(EVENT_STOP_SERVER).then((result) => {
           this.serverStarting = false;
@@ -133,6 +153,31 @@ export default defineComponent({
         window.$message.error("Proxy test failed");
       } finally {
         this.proxyTesting = false;
+      }
+    },
+    async healthCheck() {
+      console.log("Health Checking");
+      const url = `https://kh.ssl.ak.tiles.virtualearth.net/health`;
+
+      let options = {
+        timeout: {
+          request: 15 * 1000
+        },
+        rejectUnauthorized: false
+      };
+      try {
+        const resp = await got(url, options);
+
+        console.log(resp.statusCode, resp.body);
+
+        if (resp.statusCode === 200 && resp.body === "alive") {
+          this.healthCheckResult = "passed";
+        } else {
+          this.healthCheckResult = "failed";
+        }
+      } catch (ex) {
+        console.error(ex);
+        this.healthCheckResult = "failed";
       }
     }
   }
