328ab176b2690b9bf3d349e4a3837dc62e92d082
derekhe <derekhe@april1985.com>
2023-01-07 21:30:00 +0800

    Extract ServerSelection

---
+++ src/components/Debug.vue
@@ -0,0 +1,29 @@
+<template>
+  <n-h4>FAQ</n-h4>
+  <n-p>Please read <a href="https://github.com/derekhe/msfs2020-google-map/wiki/FAQ" target="_blank">FAQ</a>
+    page first
+  </n-p>
+  <n-button @click="resetToDefault">Reset to default</n-button>
+  <n-h4>Logs</n-h4>
+  <n-p>Please click "View" -> "Toggle Developer Tools" to view detailed log. More logs can be found in
+    <b>{{ logDirectory }}</b></n-p>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import log from "electron-log";
+
+const getDirectory = (path) => {
+  return path.substring(0, path.lastIndexOf("\\") + 1);
+};
+
+export default defineComponent({
+  name: "Debug",
+  data() {
+    return {
+      logDirectory: getDirectory(log.transports.file.getFile().path)
+    };
+  }
+});
+
+</script>
---
+++ src/components/ServerSelection.vue
@@ -0,0 +1,57 @@
+<template>
+  <n-radio-group
+    v-model:value="selectedServer"
+    name="radiogroup"
+    @change="updateConfig"
+  >
+    <n-space>
+      <n-radio
+        v-for="server in googleServers"
+        :key="server"
+        :value="server"
+      >
+        {{ server }}
+      </n-radio>
+    </n-space>
+  </n-radio-group>
+</template>
+
+<script>
+
+import { defineComponent } from "vue";
+import log from "electron-log";
+import Store from "electron-store";
+import got from "got";
+
+const store = new Store();
+
+export default defineComponent({
+  name: "ServerSelection",
+  props: {
+    serverStarted: Boolean
+  },
+  data() {
+    return {
+      googleServers: ["mt.google.com", "khm.google.com"],
+      selectedServer: store.get("selectedServer", "mt.google.com")
+    };
+  },
+  methods: {
+    async updateConfig() {
+      log.info("Updating config", this.selectedServer);
+      store.set("selectedServer", this.selectedServer);
+
+      if (this.serverStarted) {
+        await got.post("http://localhost:39871/configs", {
+          json: {
+            selectedServer: this.selectedServer
+          }
+        });
+      }
+
+      log.info("Updated config");
+    }
+  }
+});
+
+</script>
