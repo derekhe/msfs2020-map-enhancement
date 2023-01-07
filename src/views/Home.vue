7447ba122c8ae4ff966db31a2b6e31eb6fae9020
derekhe <derekhe@april1985.com>
2023-01-07 19:38:17 +0800

    Fix some urls

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -142,9 +142,8 @@
       >
         <n-space>
           <n-p>Author: He Sicong</n-p>
-          <a href="https://github.com/derekhe/msfs2020-google-map-electron" target="_blank">Home Page</a>
-          <a href="https://zh.flightsim.to/file/19345/msfs-2020-google-map-replacement" target="_blank">Discussion</a>
-          <a href="https://github.com/derekhe/msfs2020-google-map/issues" target="_blank">Report Issue</a>
+          <a href="https://flightsim.to/file/19345/msfs-2020-google-map-replacement" target="_blank">Home Page</a>
+          <a href="https://github.com/derekhe/msfs2020-google-map-electron/issues" target="_blank">Report Issue</a>
           <a href="https://www.paypal.com/paypalme/siconghe?country.x=C2&locale.x=en_US" target="_blank">Donate</a>
         </n-space>
       </n-layout-footer>
