694b487f9e61a2e7a5496ecd89271352e415d6c8
derekhe <derekhe@april1985.com>
2023-01-08 10:46:52 +0800

    Extract ServerCheck

--- src/components/Important.vue
+++ src/components/Important.vue
@@ -2,7 +2,7 @@
   <n-alert title="Important" type="warning">
     <n-ul>
       <n-li>Run this mod before MSFS2020, otherwise default bing map may appear randomly</n-li>
-      <n-li>If you enable rolling cache in game, please clear it before enable this mode</n-li>
+      <n-li>If you enable rolling cache in game, please clear it before enable this mod</n-li>
       <n-li>Setup proxy if your access to google is blocked</n-li>
       <n-li>Disable any firewall and antivirus if you have trouble</n-li>
     </n-ul>
