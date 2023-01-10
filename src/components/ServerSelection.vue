27e827e543f86add5e4a92975be436b1bbbf83e5
derekhe <derekhe@april1985.com>
2023-01-10 22:08:23 +0800

    Add some descriptions.

--- src/components/ServerSelection.vue
+++ src/components/ServerSelection.vue
@@ -1,4 +1,5 @@
 <template>
+  <n-p>Server can be changed on the fly when you are flying. Choose the one you like best.</n-p>
   <n-radio-group
     v-model:value="selectedServer"
     name="radiogroup"
@@ -14,6 +15,12 @@
       </n-radio>
     </n-space>
   </n-radio-group>
+  <n-h3>Google servers</n-h3>
+  <n-p>They provide the best resolution when flying low and seems very update to date. Both Google server's images are the same, choose the fastest from mt and khm server.</n-p>
+  <n-p>In some places, google servers are blocked, you should setup a proxy.</n-p>
+  <n-h3>ArcGIS</n-h3>
+  <n-p>ArcGIS server provides more natural color than Google server in some areas, but it lacks high resolution data when flying low. Some areas have different satellite images in different resolution, so you will see image changes when flying low.</n-p>
+  <n-P>ArcGIS server does not need to use a proxy.</n-P>
 </template>
 
 <script>
