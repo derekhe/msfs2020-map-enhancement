87171d2249e870059163ecf6623cd0471b144e79
derekhe <derekhe@april1985.com>
2023-05-12 22:44:10 +0800

    Add about page

--- packages/renderer/src/components/About/About.vue
+++ packages/renderer/src/components/About/About.vue
@@ -1,11 +1,30 @@
 <template>
-
+  <div class="hero h-full bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">MSFS2020 Map Enhancement</h1>
+        <p class="py-6">By He Sicong</p>
+        <div class="space-y-4 space-x-4">
+          <button class="btn btn-primary btn-sm"><a
+            href="https://zh.flightsim.to/file/19345/msfs-2020-google-map-replacement" target="_blank">Flightsim.to
+            HomePage</a>
+          </button>
+          <button class="btn btn-primary btn-sm"><a href="https://github.com/derekhe/msfs2020-map-enhancement"
+                                                    target="_blank">Github
+            Repository</a></button>
+          <button class="btn btn-primary btn-sm"><a href="https://github.com/derekhe/msfs2020-map-enhancement/releases"
+                                                    target="_blank">Github
+            Release</a></button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "About"
-}
+};
 </script>
 
 <style scoped>
