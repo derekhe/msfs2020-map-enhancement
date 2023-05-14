2b9695347892973b79450a5d434e5ba8e7788afe
derekhe <derekhe@april1985.com>
2023-05-14 14:55:48 +0800

    Fix menu item size issue Add about page and new version notification

--- packages/renderer/src/components/About/About.vue
+++ packages/renderer/src/components/About/About.vue
@@ -4,17 +4,30 @@
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">MSFS2020 Map Enhancement</h1>
         <p class="py-6">By He Sicong</p>
-        <div class="space-y-4 space-x-4">
+        <div class="divider"></div>
+        <div class="space-x-4">
           <button class="btn btn-primary btn-sm"><a
-            href="https://zh.flightsim.to/file/19345/msfs-2020-google-map-replacement" target="_blank">Flightsim.to
-            HomePage</a>
+            href="https://zh.flightsim.to/file/19345/msfs-2020-google-map-replacement" target="_blank">HomePage</a>
           </button>
           <button class="btn btn-primary btn-sm"><a href="https://github.com/derekhe/msfs2020-map-enhancement"
                                                     target="_blank">Github
             Repository</a></button>
           <button class="btn btn-primary btn-sm"><a href="https://github.com/derekhe/msfs2020-map-enhancement/releases"
-                                                    target="_blank">Github
-            Release</a></button>
+                                                    target="_blank">Old
+            Releases</a></button>
+        </div>
+        <div class="divider"></div>
+        <div class="indicator" v-if="this.statusStore.updateInfo">
+          <div class="indicator-item indicator-bottom">
+            <button class="btn btn-primary"><a href="https://flightsim.to/file/19345/msfs-2020-google-map-replacement"
+                                               target="_blank">Update</a></button>
+          </div>
+          <div class="card border">
+            <div class="card-body">
+              <h2 class="card-title">New version: {{ releaseVersion }}</h2>
+              <div v-html="releaseNotes"></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -22,8 +35,24 @@
 </template>
 
 <script>
+import { useStatusStore } from "../../stores/statusStore";
+
 export default {
-  name: "About"
+  name: "About",
+  data() {
+    let statusStore = useStatusStore();
+    return {
+      statusStore
+    };
+  },
+  computed: {
+    releaseVersion() {
+      return this.statusStore.updateInfo ? this.statusStore.updateInfo.releaseName : "";
+    },
+    releaseNotes() {
+      return this.statusStore.updateInfo ? this.statusStore.updateInfo.releaseNotes : "";
+    }
+  }
 };
 </script>
 
